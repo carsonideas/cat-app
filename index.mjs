952f629270f586de6d933f9d269fc514cd090ba5
Ideahub <douglascarson1121@gmail.com>
2025-05-29 08:17:52 +0300

    fist path issue in deployment

--- index.mjs
+++ index.mjs
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const myFactsInput = document.querySelector('#factsInput');
-const myPhotosInput = document.querySelector('#photosInput');
-const myFactsSubmitButton = document.querySelector('.submit_button1');
-const myPhotosSubmitButton = document.querySelector('.submit_button2');
+const myFactsInput = document.querySelector("#factsInput");
+const myPhotosInput = document.querySelector("#photosInput");
+const myFactsSubmitButton = document.querySelector(".submit_button1");
+const myPhotosSubmitButton = document.querySelector(".submit_button2");
 
 let myFactsResultContainer = document.querySelector(".facts-result");
 let myPhotosResultContainer = document.querySelector(".photos-result");
@@ -31,48 +31,44 @@ if (!myPhotosResultContainer) {
   document.body.appendChild(myPhotosResultContainer);
 }
 
-
-
 myFactsSubmitButton.addEventListener("click", async (e) => {
   e.preventDefault();
 
-  
   const factsCount = parseInt(myFactsInput.value);
-  myPhotosResultContainer.innerHTML = ""; 
+  myPhotosResultContainer.innerHTML = "";
 
   if (!(factsCount >= 1 && factsCount <= 50)) {
     myFactsResultContainer.innerHTML = `<p style="color: red;">Enter a number between 1 and 50</p>`;
-    return; 
+    return;
   }
 
   myFactsResultContainer.style.marginTop = "1rem";
 
   myFactsResultContainer.innerHTML = `<p style="font-size: 1.4rem; color: var(--accent-color);">Please wait, fetching cat facts.....</p>`;
-  myFactsSubmitButton.setAttribute("disabled", true); 
+  myFactsSubmitButton.setAttribute("disabled", true);
 
   try {
-
-
-
     // const res = fetch(`https://meowfacts.herokuapp.com/?count=${factsCount}`);
     // fetch(`https://meowfacts.herokuapp.com/?count=${factsCount}`)
     //  .then(response => response.json())
     //  .then(data => { /* use data */ });
 
-
-
-    const res = await axios.get(`https://meowfacts.herokuapp.com/?count=${factsCount}`);
-    const list = res.data.data.map(fact =>
-      `<li style="margin-bottom: 1rem; font-size: 1.2rem;">${fact}</li>`
-    ).join('');
+    const res = await axios.get(
+      `https://meowfacts.herokuapp.com/?count=${factsCount}`
+    );
+    const list = res.data.data
+      .map(
+        (fact) =>
+          `<li style="margin-bottom: 1rem; font-size: 1.2rem;">${fact}</li>`
+      )
+      .join("");
     myFactsResultContainer.innerHTML = `<ol style="margin-left: 2rem; margin-top: 1rem;">${list}</ol>`;
   } catch (error) {
+    // console.log(error);
 
-     // console.log(error);
-    
     myFactsResultContainer.innerHTML = `<p style="color:red;">Houston, we have a problem!!! Something went wrong fetching cat facts. Please try again.</p>`;
   } finally {
-    myFactsSubmitButton.removeAttribute("disabled"); 
+    myFactsSubmitButton.removeAttribute("disabled");
   }
 });
 
@@ -80,9 +76,8 @@ myPhotosSubmitButton.addEventListener("click", async (e) => {
   e.preventDefault();
 
   const photosCount = parseInt(myPhotosInput.value);
-  myFactsResultContainer.innerHTML = ""; 
+  myFactsResultContainer.innerHTML = "";
 
-  
   if (!(photosCount >= 1 && photosCount <= 10)) {
     myPhotosResultContainer.innerHTML = `<p style="color: red;">Enter a number between 1 and 10</p>`;
     return;
@@ -92,14 +87,19 @@ myPhotosSubmitButton.addEventListener("click", async (e) => {
   myPhotosSubmitButton.setAttribute("disabled", true);
 
   try {
-   
+    // const res = await axios.get("https://api.thecatapi.com/v1/images/search");
 
-     // const res = await axios.get("https://api.thecatapi.com/v1/images/search");
-     
-    const res = await axios.get(`https://api.thecatapi.com/v1/images/search?limit=${photosCount}`);
-    myPhotosResultContainer.innerHTML = res.data.map((photo, i) =>
-      `<div><img src="${photo.url}" alt="Cat ${i + 1}" style="width:100%; height:200px;"></div>`
-    ).join('');
+    const res = await axios.get(
+      `https://api.thecatapi.com/v1/images/search?limit=${photosCount}`
+    );
+    myPhotosResultContainer.innerHTML = res.data
+      .map(
+        (photo, i) =>
+          `<div><img src="${photo.url}" alt="Cat ${
+            i + 1
+          }" style="width:100%; height:200px;"></div>`
+      )
+      .join("");
   } catch (error) {
     myPhotosResultContainer.innerHTML = `<p style="color:red;">Houston, we have a problem!!! Something went wrong fetching cat photos. Please try again.</p>`;
   } finally {
